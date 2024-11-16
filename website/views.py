f8fc0508376c6b8403ff91fb5c07b8dea9a5ac3b
CYR3S <amingholami06@gmail.com>
2024-11-16 14:15:57 +0330

    Add static dirs and url dir
    -Static root and url
    -Media root and url
    -User jinja

--- website/views.py
+++ website/views.py
@@ -6,10 +6,10 @@ def index_view(request):
     return render(request, 'website/index.html')
 
 def about_view(request):
-    return render(request, 'about.html')
+    return render(request, 'website/about.html')
 
 def contact_view(request):
-    return render(request, 'contact.html')
+    return render(request, 'website/contact.html')
 
 def json_test(request):
     return JsonResponse({"Name" : "Ali"})
