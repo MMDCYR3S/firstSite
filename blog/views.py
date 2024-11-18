ae05d90f3e764f1b629a815fa9f8cf312a9c6798
CYR3S <amingholami06@gmail.com>
2024-11-18 16:31:37 +0330

    Blog
    Get some changes to blog

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 def blog_view(request):
     return render(request, "blog/blog.html")
 def blog_single(request):
-    return render(request, "blog/blog-details.html")
+    context = {"title": "I don't have a fucking clue!", "content": "What do you want me to say? Fuck Off!", "author": "Mamad CYR3S"}
+    return render(request, "blog/blog-details.html", context)
